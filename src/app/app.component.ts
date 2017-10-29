ae8a63d16ce6355a704143ca1b1f41f796d1d3af
Miguel Angel Alvarez <malvarez@desarrolloweb.com>
2017-10-29 10:22:25 +0100

    Ejemplo básico de conexión Ajax en Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  heroes: any;
+
+  constructor(private http: Http) { }
+
+  ngOnInit() {
+    const marvelPublicApiKey = 'e7c7a8f3d7d409ade75377d62de644ed';
+    const url = `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=spider&apikey=${marvelPublicApiKey}`;
+    const httpObservable = this.http.get(url);
+    httpObservable.subscribe(
+      (response) => {
+        const datos = response.json();
+        console.log(datos.data.results);
+        this.heroes = datos.data.results;
+      }
+    );
+  }
 }
